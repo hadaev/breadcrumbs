548e0233924dcff051da33d8abab2dbad883d26e
DESKTOP-LA1799I\hadae <alex.hadaev@n3wnormal.com>
2019-10-22 17:07:10 +0300

    add new option

--- admin/class-plugin-breadcrumbs-admin.php
+++ admin/class-plugin-breadcrumbs-admin.php
@@ -1,5 +1,5 @@
 <?php
-
+$true_page = 'breadcrumbs';
 /**
  * The admin-specific functionality of the plugin.
  *
@@ -47,11 +47,15 @@ class Breadcrumbs_Admin {
 	 * @param      string    $Breadcrumbs       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
+//	public $true_page;
 	public function __construct( $Breadcrumbs, $version ) {
 
 		$this->Breadcrumbs = $Breadcrumbs;
 		$this->version = $version;
-		add_action( 'admin_menu', array($this, 'breadcrumbs_CreateAdminMenu') );
+//		$this->true_page = 'myparameters.php';
+		add_action( 'admin_init', array($this, 'true_option_settings') );
+		add_action( 'admin_menu', array($this, 'true_options') );
+
 	}
 
 	/**
@@ -109,15 +113,111 @@ class Breadcrumbs_Admin {
 			array($this, 'breadcrumbsOptions')
 		);
 	}
+
+	public function true_options() {
+		global $true_page;
+		add_options_page( 'Параметры',
+			'Параметры',
+			'manage_options',
+			'breadcrumbs',
+			array($this, 'true_option_page'));
+	}
+	public function true_option_page(){
+
+		?><div class="wrap">
+		<h2>Хлебные крошки</h2>
+		<form method="post" enctype="multipart/form-data" action="options.php">
+			<?php
+			settings_fields('true_options'); // меняем под себя только здесь (название настроек)
+			do_settings_sections('breadcrumbs');
+			?>
+			<p class="submit">
+				<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			</p>
+		</form>
+		</div><?php
+	}
+	public function true_validate_settings($input) {
+		foreach($input as $k => $v) {
+			$valid_input[$k] = trim($v);
+
+			/* Вы можете включить в эту функцию различные проверки значений, например
+			if(! задаем условие ) { // если не выполняется
+				$valid_input[$k] = ''; // тогда присваиваем значению пустую строку
+			}
+			*/
+		}
+		return $valid_input;
+	}
+	function true_option_settings() {
+		// Присваиваем функцию валидации ( true_validate_settings() ). Вы найдете её ниже
+		register_setting( 'true_options', 'true_options', array($this, 'true_validate_settings') ); // true_options
+
+		// Добавляем секцию
+		add_settings_section( 'true_section_1', 'Текстовые поля ввода', '', 'breadcrumbs' );
+
+		// Создадим текстовое поле в первой секции
+		$true_field_params = array(
+			'type'      => 'text', // тип
+			'id'        => 'my_text',
+			'desc'      => 'Пример обычного текстового поля.', // описание
+			'label_for' => 'my_text' // позволяет сделать название настройки лейблом (если не понимаете, что это, можете не использовать), по идее должно быть одинаковым с параметром id
+		);
+		add_settings_field( 'my_text_field', 'Текстовое поле', array($this, 'true_option_display_settings'), 'breadcrumbs', 'true_section_1', $true_field_params );
+
+		// Создадим textarea в первой секции
+		$true_field_params = array(
+			'type'      => 'textarea',
+			'id'        => 'my_textarea',
+			'desc'      => 'Пример большого текстового поля.'
+		);
+		add_settings_field( 'my_textarea_field', 'Большое текстовое поле', array($this, 'true_option_display_settings'), 'breadcrumbs', 'true_section_1', $true_field_params );
+
+		// Добавляем вторую секцию настроек
+
+		add_settings_section( 'true_section_2', 'Другие поля ввода', '', 'breadcrumbs' );
+
+		// Создадим чекбокс
+		$true_field_params = array(
+			'type'      => 'checkbox',
+			'id'        => 'my_checkbox',
+			'desc'      => 'Пример чекбокса.'
+		);
+		add_settings_field( 'my_checkbox_field', 'Чекбокс', array($this, 'true_option_display_settings'), 'breadcrumbs', 'true_section_2', $true_field_params );
+
+		// Создадим выпадающий список
+		$true_field_params = array(
+			'type'      => 'select',
+			'id'        => 'my_select',
+			'desc'      => 'Пример выпадающего списка.',
+			'vals'		=> array( 'val1' => 'Значение 1', 'val2' => 'Значение 2', 'val3' => 'Значение 3')
+		);
+		add_settings_field( 'my_select_field', 'Выпадающий список', array($this, 'true_option_display_settings'), 'breadcrumbs', 'true_section_2', $true_field_params );
+
+		// Создадим радио-кнопку
+		$true_field_params = array(
+			'type'      => 'radio',
+			'id'      => 'my_radio',
+			'vals'		=> array( 'val1' => 'Значение 1', 'val2' => 'Значение 2', 'val3' => 'Значение 3')
+		);
+		add_settings_field( 'my_radio', 'Радио кнопки', array($this, 'true_option_display_settings'), 'breadcrumbs', 'true_section_2', $true_field_params );
+
+	}
+	public function true_option_display_settings($args) {
+		extract( $args );
+		include(plugin_dir_path(__FILE__).'settings.php');
+
+	}
 	public function breadcrumbsOptions()
 	{
 		if (!current_user_can( 'manage_options' ))
 			wp_die( __( 'You do not have sufficient permissions to access this page.','simple_basket') );
 
-		include(plugin_dir_path(__FILE__).'settings.php');
-	}
 
+	}
 
 
 }
 
+$all_options = get_option('true_options'); // это массив
+var_dump($all_options); ;
--- admin/settings.php
+++ admin/settings.php
@@ -1,118 +1,101 @@
 <?php
-// Параметры плагина
-add_option('simple_basket_buynow_caption', __('Buy', 'simple_basket'));
-add_option('simple_basket_order_page', '');
-add_option('simple_basket_catalog_price', __('Price', 'simple_basket'));
-add_option('simple_basket_delivery', '0');
-add_option('simple_basket_delivery_default', '0');
-add_option('simple_basket_google_analytics_mode', '0');
-add_option('simple_basket_order_compete_virtual_page', '');
-add_option('simple_basket_conformation_email_post', '');
-add_option('simple_basket_conformation_email_subject', '');
-add_option('simple_basket_admin_email_post', '');
-add_option('simple_basket_admin_email_subject', '');
 
+$option_name = 'true_options';
 
-// Принимаем данные
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-	if (isset($_POST['bc_position']))
-		update_option('bc_position', $_POST['bc_position']);
+$o = get_option( $option_name );
 
-	if (isset($_POST['orderpage']))
-		update_option('simple_basket_order_page', $_POST['orderpage']);
-
-	if (isset($_POST['pricefield']))
-		update_option('simple_basket_catalog_price', $_POST['pricefield']);
-
-	if (isset($_POST['deliverymode']))
-		update_option('simple_basket_delivery', '1');
-	else
-		update_option('simple_basket_delivery', '0');
-
-	if (isset($_POST['deliveryplan']))
-		update_option('simple_basket_delivery_default', $_POST['deliveryplan']);
-
-	if (isset($_POST['googleanalyticsmode']))
-		update_option('simple_basket_google_analytics_mode', $_POST['googleanalyticsmode']);
-
-	if (isset($_POST['ordercompletevirtualvage']))
-		update_option('simple_basket_order_compete_virtual_page', $_POST['ordercompletevirtualvage']);
-
-
-	if (isset($_POST['confirmationEmailSubject']))
-		update_option('simple_basket_conformation_email_subject', $_POST['confirmationEmailSubject']);
-	if (isset($_POST['confirmemail']))
-		update_option('simple_basket_conformation_email_post', $_POST['confirmemail']);
-
-	if (isset($_POST['adminEmailSubject']))
-		update_option('simple_basket_admin_email_subject', $_POST['adminEmailSubject']);
-	if (isset($_POST['adminemail']))
-		update_option('simple_basket_admin_email_post', $_POST['adminemail']);
-
+switch ( $type ) {
+	case 'text':
+		$o[$id] = esc_attr( stripslashes($o[$id]) );
+		echo "<input class='regular-text' type='text' id='$id' name='" . $option_name . "[$id]' value='$o[$id]' />";
+		echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+		break;
+	case 'textarea':
+		$o[$id] = esc_attr( stripslashes($o[$id]) );
+		echo "<textarea class='code large-text' cols='50' rows='10' type='text' id='$id' name='" . $option_name . "[$id]'>$o[$id]</textarea>";
+		echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+		break;
+	case 'checkbox':
+		$checked = ($o[$id] == 'on') ? " checked='checked'" :  '';
+		echo "<label><input type='checkbox' id='$id' name='" . $option_name . "[$id]' $checked /> ";
+		echo ($desc != '') ? $desc : "";
+		echo "</label>";
+		break;
+	case 'select':
+		echo "<select id='$id' name='" . $option_name . "[$id]'>";
+		foreach($vals as $v=>$l){
+			$selected = ($o[$id] == $v) ? "selected='selected'" : '';
+			echo "<option value='$v' $selected>$l</option>";
+		}
+		echo ($desc != '') ? $desc : "";
+		echo "</select>";
+		break;
+	case 'radio':
+		echo "<fieldset>";
+		foreach($vals as $v=>$l){
+			$checked = ($o[$id] == $v) ? "checked='checked'" : '';
+			echo "<label><input type='radio' name='" . $option_name . "[$id]' value='$v' $checked />$l</label><br />";
+		}
+		echo "</fieldset>";
+		break;
 }
+/*
+ * Функция проверки правильности вводимых полей
+ */
+
+// Параметры плагина
+//add_option('bc_position', __('left'));
+//add_option('simple_basket_order_page', '');
+//add_option('simple_basket_catalog_price', __('Price', 'simple_basket'));
+//add_option('simple_basket_delivery', '0');
+//add_option('simple_basket_delivery_default', '0');
+//add_option('simple_basket_google_analytics_mode', '0');
+//add_option('simple_basket_order_compete_virtual_page', '');
+//add_option('simple_basket_conformation_email_post', '');
+//add_option('simple_basket_conformation_email_subject', '');
+//add_option('simple_basket_admin_email_post', '');
+//add_option('simple_basket_admin_email_subject', '');
+//
+//
+//// Принимаем данные
+//if ($_SERVER['REQUEST_METHOD'] == 'POST')
+//{
+//	if (isset($_POST['bc_position']))
+//		update_option('bc_position', $_POST['bc_position']);
+//
+//	if (isset($_POST['orderpage']))
+//		update_option('simple_basket_order_page', $_POST['orderpage']);
+//
+//	if (isset($_POST['pricefield']))
+//		update_option('simple_basket_catalog_price', $_POST['pricefield']);
+//
+//	if (isset($_POST['deliverymode']))
+//		update_option('simple_basket_delivery', '1');
+//	else
+//		update_option('simple_basket_delivery', '0');
+//
+//	if (isset($_POST['deliveryplan']))
+//		update_option('simple_basket_delivery_default', $_POST['deliveryplan']);
+//
+//	if (isset($_POST['googleanalyticsmode']))
+//		update_option('simple_basket_google_analytics_mode', $_POST['googleanalyticsmode']);
+//
+//	if (isset($_POST['ordercompletevirtualvage']))
+//		update_option('simple_basket_order_compete_virtual_page', $_POST['ordercompletevirtualvage']);
+//
+//
+//	if (isset($_POST['confirmationEmailSubject']))
+//		update_option('simple_basket_conformation_email_subject', $_POST['confirmationEmailSubject']);
+//	if (isset($_POST['confirmemail']))
+//		update_option('simple_basket_conformation_email_post', $_POST['confirmemail']);
+//
+//	if (isset($_POST['adminEmailSubject']))
+//		update_option('simple_basket_admin_email_subject', $_POST['adminEmailSubject']);
+//	if (isset($_POST['adminemail']))
+//		update_option('simple_basket_admin_email_post', $_POST['adminemail']);
+//
+//}
 
 
 // Для списка SELECT
 ?>
-<div id="simple_basket">
-	<h1><?php
-		_e('Breadcrumbs', 'breadcrumbs');
-		?></h1>
-    <h3><?php _e('Settings')?></h3>
-	<form method="post" action="">
-		<div id="bc-main-container">
-			<div id="bs-section">
-				<fieldset>
-					<?php $bc_position = 'left' ?>
-					<?php if(get_option('bc_position'))$bc_position=get_option('bc_position'); ?>
-					<?php var_dump(get_option('bc_position')); ?>
-					<legend><?php _e('Breadcrumbs position', 'breadcrumbs')?></legend>
-					<div class="control">
-						<label for="left_position"><?php _e('Left position', 'breadcrumbs')?></label>
-						<input
-                                id="left_position"
-                                class="textString"
-                                type="radio"
-                                name="name_position"
-                                <?= $bc_position=='left'?'checked': ''?>
-                                value="left" />
-                        <br>
-                        <label for="right_position"><?php _e('Right position', 'breadcrumbs')?></label>
-						<input id="right_position" class="textString" type="radio" name="name_position" <?= $bc_position=='right'?'checked': ''?> value="right" />
-                        <br>
-                        <label for="center_position"><?php _e('Center position', 'breadcrumbs')?></label>
-						<input id="center_position" class="textString" type="radio" name="name_position" <?= $bc_position=='center'?'checked': ''?> value="center" />
-						<p><?php _e('Choose a position for bread crumbs', 'breadcrumbs')?></p>
-                        <script>
-
-                            $('#bs-section input').change(function () {
-                                // console.log($(this).prop());
-                                console.log($(this).val());
-                                console.log($(this).is());
-                                // console.log($(this).attr());
-                            });
-                        </script>
-					</div>
-<!--					<div class="control">-->
-<!--						<label for="orderPage">--><?php //_e('Order Page', 'simple_basket')?><!--</label>-->
-<!--						<input id="orderPage" class="textString" type="text" name="orderpage" value="--><?php //echo get_option('simple_basket_order_page'); ?><!--" />-->
-<!--						<p>--><?php //_e('This parameter specifies the URL of page contains order form. To display thу order form use shortcode [basket-order-form]', 'simple_basket')?><!--</p>-->
-<!--					</div>-->
-<!--				</fieldset>-->
-<!--				<fieldset>-->
-<!--					<legend>--><?php //_e('Product Catalog', 'simple_basket')?><!--</legend>-->
-<!--					<div class="control">-->
-<!--						<label for="priceCustomFiled">--><?php //_e('Price Custom Field', 'simple_basket')?><!--</label>-->
-<!--						<input id="priceCustomFiled" class="textString" type="text" name="pricefield" value="--><?php //echo get_option('simple_basket_catalog_price'); ?><!--" />-->
-<!--						<p>--><?php //_e('This parameter specifies the name of product custom field that contains the price.', 'simple_basket')?><!--</p>-->
-<!--					</div>-->
-<!--				</fieldset>-->
-
-			</div><!--/tabSBMain-->
-		</div><!--/tabSB-->
-		<div>
-			<button class="button button-primary" type="submit"><?php _e('Update settings', 'breadcrumbs')?></button>
-		</div>
-	</form>
-</div>
